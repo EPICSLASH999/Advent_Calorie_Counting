c217e1ecc73533205c711a25f35f9a2792508a8c
Jesus Santos <jesus.santos364@gmail.com>
2023-05-30 13:21:58 -0700

    first commit

---
+++ bin/calorie_counting.dart
@@ -0,0 +1,5 @@
+import 'package:calorie_counting/calorie_counting.dart' as calorie_counting;
+
+void main(List<String> arguments) {
+  
+}
---
+++ test/calorie_counting_test.dart
@@ -0,0 +1,6 @@
+import 'package:calorie_counting/calorie_counting.dart';
+import 'package:test/test.dart';
+
+void main() {
+  
+}
