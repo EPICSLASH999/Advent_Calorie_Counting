c65aa901c5b030c4741fb3e5ab45522074c32dfa
Jesus Santos <jesus.santos364@gmail.com>
2023-05-30 13:47:58 -0700

    Funcion para leer archivo

--- test/calorie_counting_test.dart
+++ test/calorie_counting_test.dart
@@ -1,6 +1,16 @@
+import 'dart:io';
+
 import 'package:calorie_counting/calorie_counting.dart';
 import 'package:test/test.dart';
 
 void main() {
-  
+  test('Leer primera linea de .txt', () {
+
+    var x = leerFile();
+    print(x);
+  });
+}
+
+leerFile(){
+  return File("C:/Users/M1-MQ3/Documents/Dart_Proyects/examen_calorias/calorie_counting/puzzle_1.txt").readAsLinesSync();
 }
